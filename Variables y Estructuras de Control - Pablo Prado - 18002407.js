14d1dc6f8425633adb18adddc1c313251784dc84
Pablo Prado <pradopablo502@gmail.com>
2022-02-07 09:13:21 -0600

    first commit

---
+++ Variables y Estructuras de Control - Pablo Prado - 18002407.js
@@ -0,0 +1,15 @@
+const carnet = "18002407"
+const nombrecompleto = "Pablo Prado"
+var mes = 10
+
+if (mes=12 || 1 || 2) {
+    alert ("es invierno")
+} else if(mes >= 3 && mes <=5){
+    alert ("es primavera")
+} else if(mes >=6 && mes <=8){
+    alert ("es verano")
+} else if(mes >=9 && mes <=11){
+    alert ("es otoño")
+}
+
+alert ("Número de carné: " + carnet + " Nombre Completo: " + nombrecompleto)
